d437748f0eccecd755c2f7561f4f251d137c14bb
Volodymyr Kolomiiets <valdi6014@gmail.com>
2025-05-16 23:59:33 +0300

    Solution

--- todolist/settings.py
+++ todolist/settings.py
@@ -27,6 +27,13 @@ ALLOWED_HOSTS = []
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
+ENGINE = os.environ.get("ENGINE", "")
+NAME = os.environ.get("NAME", "")
+USER = os.environ.get("USER", "")
+PASSWORD = os.environ.get("PASSWORD", "")
+HOST = os.environ.get("HOST", "")
+PORT = os.environ.get("PORT", "")
+
 # Application definition
 
 INSTALLED_APPS = (
@@ -62,12 +69,12 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'app_db',
-        'USER': 'app_user',
-        'PASSWORD': '1234',
-        'HOST': 'mysql',  # You can use a different host if your MySQL server is on a remote machine.
-        'PORT': '',  # Leave this empty to use the default MySQL port (3306).
+        'ENGINE': ENGINE,
+        'NAME': NAME,
+        'USER': USER,
+        'PASSWORD': PASSWORD,
+        'HOST': HOST,  # You can use a different host if your MySQL server is on a remote machine.
+        'PORT': PORT,  # Leave this empty to use the default MySQL port (3306).
     }
 }
 
